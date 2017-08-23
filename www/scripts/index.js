0d1c5212865108aa0f350e465d5ed2784dffa99e
shaimuli <shaimuli@gmail.com>
2017-08-23 22:23:13 +0300

    567
    567

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -8,7 +8,7 @@
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
-        var ref = window.open('https://app.only-numbers.com/', '_self', 'location=yes');
+        var ref = window.open('https://app.only-numbers.com/', '_self', 'location=no');
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
